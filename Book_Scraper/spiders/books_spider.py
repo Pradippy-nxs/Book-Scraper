b9fffe1a079af3b4286bf52888f02926a18c8200
dipta <mpradipta.arya03@gmail.com>
2025-11-15 22:36:34 +0700

    Added main crawler code and cleaned up everything

--- Book_Scraper/spiders/books_spider.py
+++ Book_Scraper/spiders/books_spider.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import scrapy
 
-strArr_genres = ["Travel",
+genres = ["Travel",
     "Mystery",
     "Historical Fiction",
     "Sequential Art",
@@ -53,29 +53,73 @@ strArr_genres = ["Travel",
     "Erotica",
     "Crime"]
 
-str_link_prefix = "https://books.toscrape.com/catalogue/category/books/"
-str_link_postfix = "/index.html"
+# Link base 
+link_prefix = "https://books.toscrape.com/catalogue/category/books/"
+link_postfix = "/index.html"
 
-def strArr_link_maker():
-    strArr_links = []
-    for i in range(len(strArr_genres)):
-        strArr_links.append(str_link_prefix + str(strArr_genres[i].lower())+ "_" + str((i+2))+str_link_postfix)
-    return strArr_links
+# Replace spaces with dashes 
+# Can be refactored to be more performant/clean
+def space_replacer(genres):
+    filtered_genres = []
+    for genre in genres:
+        temp = ""
+        for character in genre:
+            if character == ' ':
+                temp += '-'
+            else:
+                temp += character
+        filtered_genres.append(temp)
+    return filtered_genres
 
+# Makes the link for the spider to use
+def link_maker(filtered):
+    links = []
+    for i in range(len(filtered)):
+        links.append(link_prefix + str(filtered[i].lower())+ "_" + str((i+2))+link_postfix)
+    return links
 
-print(strArr_link_maker())
+#Filter function to see the rating
+def rating_filter(rating):
+    # quantity = ["One", "Two", "Three", "Four", "Five"]
+    word_to_delete = "star-rating "
+    current_word = ""
+    for character in rating:
+        # if current_word not in quantity:
+        if current_word == word_to_delete:
+            current_word = ""
+        current_word += character
+    return current_word + " star"
+# print(link_maker(space_replacer(genres)))
 
+def status_filter(status):
+    word_to_return = "instock"
+    current_word = ""
+    for character in status:
+        if current_word != word_to_return:
+            current_word += character
+        else:
+            return "In stock"
 
 class QuotesSpider(scrapy.Spider):
 
     name = "books"
-    async def start(self):
-        urls = strArr_link_maker()
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    start_urls = ["https://books.toscrape.com/catalogue/category/books/crime_51/index.html"]
+    # start_urls = link_maker(space_replacer(genres))
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f"books-{page}.html"
-        Path(filename).write_bytes(response.body)
-        self.log(f"Saved file {filename}")
+
+        # Generate html files 
+        # page = response.url.split("/")[-2]
+        # filename = f"books-{page}.html"
+        # Path(filename).write_bytes(response.body)
+
+        # Parse data 
+        genre = response.css("div ul.breadcrumb li.active::text").get()
+        for book in response.css("article.product_pod"):
+            yield {
+                "Title": book.css("h3 a::attr(title)").get(),
+                "Rating": rating_filter(book.css("p.star-rating::attr(class)").get()),
+                "Genre": genre,
+                "Price": book.css("p.price_color::text").get(),
+                "Status": status_filter(book.css("p.instock::attr(class)").get()),
+            }
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-bikin venv ajah trus install package
+File structure:
+```
+Book_Scraper/
+│
+├── Book_Scraper/
+│   ├── spiders/
+│   │   ├── books_spider.py
+│   │   ├── __init__.py
+│   │   └── __pycache__/ (.gitignored)
+│   │
+│   ├── items.py
+│   ├── middlewares.py
+│   ├── pipelines.py
+│   ├── settings.py
+│   ├── __init__.py
+│   └── __pycache__/ (.gitignored)
+│
+├── venv/ (.gitignored)
+│
+├── scrapy.cfg
+├── requirements.txt
+├── README.md
+└── .gitignore
+
+```
